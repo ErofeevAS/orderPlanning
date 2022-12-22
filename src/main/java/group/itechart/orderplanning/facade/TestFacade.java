86d89988f231df545a8040c604b34bc39eac3f2f
Alexei Erofeev <spokeman152@gmail.com>
2022-12-22 15:38:00 +0300

    test

---
+++ src/main/java/group/itechart/orderplanning/facade/TestFacade.java
@@ -0,0 +1,10 @@
+package group.itechart.orderplanning.facade;
+
+import group.itechart.orderplanning.service.dto.OrderDto;
+
+
+public interface TestFacade {
+
+	OrderDto createOrder(OrderDto orderDto);
+
+}
